de55fb93c9f8db9cd9b14079cd4399044e42d627
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-10-09 08:58:25 +0900

    Add files via upload

--- simurgh.cpp
+++ simurgh.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-//#include "simurgh.h"
+#include "simurgh.h"
 
 using namespace std;
 
@@ -13,9 +13,6 @@ typedef pair<ll, ll> pll;
 #define INF 987654321
 #define IINF 2987654321987654321
 #define MOD 1000000007
-
-std::vector<int> find_roads(int n, std::vector<int> u, std::vector<int> v);
-int count_common_roads(const std::vector<int>& r);
 
 struct Edge {
 	int to, id;
@@ -42,14 +39,18 @@ void dfs(int v, int p) {
 	}
 }
 
-int treeVal, royal[MAXN*MAXN];
-void findRoyal(int n, vector<int> &u, vector<int> &v) {
+
+
+int treeVal, royal[MAXN*MAXN], val[MAXN];
+void findRoyalInTree(int n, vector<int> &_u, vector<int> &_v) {
 	vector<int> r;
 	for(int v = 1; v < n; v++)
 		r.push_back(pa[v].id);
 	treeVal = count_common_roads(r);
 
 	for(int v = 1; v < n; v++) {
+		if(royal[pa[v].id] != -1) continue;
+
 		if(low[v].second == -1) {
 			royal[pa[v].id] = 1;
 			continue;
@@ -57,9 +58,9 @@ void findRoyal(int n, vector<int> &u, vector<int> &v) {
 		
 		int newEdge = low[v].second;
 		r.push_back(newEdge);
-
-		int x = u[newEdge], y = v[newEdge];	// x-y is back-edge
+		int x = _u[newEdge], y = _v[newEdge];	// x-y is back-edge
 		if(num[x] > num[y]) swap(x, y);		// x is ancestor
+
 		int t = y;
 		while(t != x) {
 			if(royal[pa[t].id] != -1) {
@@ -77,10 +78,76 @@ void findRoyal(int n, vector<int> &u, vector<int> &v) {
 			royal[pa[v].id] = treeVal - (count_common_roads(r) - royal[newEdge]);
 			r.push_back(pa[v].id);
 		} else {
-			
+			int t = y, mx = treeVal;
+			while(t != x) {
+				int ei = pa[t].id;
+				r.erase(find(r.begin(), r.end(), ei));
+				val[t] = count_common_roads(r);
+				r.push_back(ei);
+
+				mx = max(mx, val[t]);
+				t = pa[t].to;
+			}
+
+			t = y;
+			while(t != x) {
+				royal[pa[t].id] = mx - val[t];
+				t = pa[t].to;
+			}
 		}
+
+		r.erase(find(r.begin(), r.end(), newEdge));
 	}
 }
+
+
+
+bool vis[MAXN];
+void dfsTree(int v, int p) {
+	vis[v] = true;
+	for(auto c : tree[v]) if(!vis[c.to]) {
+		pa[c.to] = {v, c.id};
+		dfsTree(c.to, v);
+	}
+}
+
+void solve(int n, int r) {
+	memset(vis, 0, sizeof vis);
+	dfsTree(r, -1);
+	vector<Edge> ev;
+	for(auto c : adj[r])
+		if(royal[c.id] == -1) ev.push_back(c);
+
+	int start = 0, sz = (int)ev.size();
+	while(start < sz) {
+		int low = start, high = sz-1, res = -1;
+		while(low <= high) {
+			memset(vis, 0, sizeof vis);
+			int mid = (low + high) / 2, curVal = treeVal;
+			vector<int> q;
+			for(int i = 0; i <= mid; i++) {
+				q.push_back(ev[i].id);
+				vis[ev[i].to] = true;
+				curVal -= royal[pa[ev[i].to].id];
+			}
+			for(int i = 0; i < n; i++)
+				if(i != r && !vis[i]) q.push_back(pa[i].id);
+
+			if(count_common_roads(q) > curVal) {
+				res = mid;
+				high = mid-1;
+			} else {
+				low = mid+1;
+			}
+		}
+
+		if(res == -1) break;
+		royal[ev[res].id] = 1;
+		start = res + 1;
+	}
+}
+
+
 
 vector<int> find_roads(int n, vector<int> u, vector<int> v) {
 	memset(royal, -1, sizeof royal);
@@ -96,18 +163,14 @@ vector<int> find_roads(int n, vector<int> u, vector<int> v) {
 	dfs(0, -1);
 
 	// find royal road in dfs tree
+	findRoyalInTree(n, u, v);
 
+	// find every royal roads
+	for(int root = 1; root <= n; root++)
+		solve(n, root);
 
-	return vector<int>();
-}
-
-
-
-int main() {
-	find_roads(4, {0, 0, 0, 1, 1, 2}, {1, 2, 3, 2, 3, 3});
-	for(int i = 0; i < 4; i++) {
-		printf("%d: ", i);
-		for(auto e : tree[i]) printf("%d ", e.to);
-		printf("\n");
-	}
+	vector<int> rv;
+	for(int i = 0; i < m; i++)
+		if(royal[i] == 1) rv.push_back(i);
+	return rv;
 }
